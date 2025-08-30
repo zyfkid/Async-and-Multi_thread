924b78033c2952fd1992bd7563f8cfd1cf4c780a
zyfkid <yf-zhu24@mails.tsinghua.edu.cn>
2025-08-30 15:47:44 +0800

    朱羿帆2024013216完成作业

--- homework.cs
+++ homework.cs
@@ -32,6 +32,23 @@ public class Database
 
         // TODO: 使用 lock 语句保证线程安全
         // 提示：在 lock 块中查找书籍并更新库存，若库存不足则输出提示
+        lock (_lock)
+        {
+            var book = _books.Find(b => b.Title == title);
+            if (book == null)
+            {
+                Console.WriteLine($"没有《{title}》这本书");
+            }
+            else if (quantity > book.Inventory)
+            {
+                Console.WriteLine($"《{title}》库存不足，当前库存{book.Inventory}本");
+            }
+            else
+            {
+                book.Inventory -= quantity;
+                Console.WriteLine($"购买成功，《{title}》剩余库存{book.Inventory}本");
+            }
+        }
     }
 }
 
@@ -42,6 +59,7 @@ public class BookStore
     // TODO: 实现异步购书方法CheckoutAsync，调用 UpdateInventoryAsync
     public async Task CheckoutAsync(string bookTitle, int quantity)
     {
+        await _db.UpdateInventoryAsync(bookTitle, quantity);
     }
 
     public async Task SimulateMultipleUsers()
@@ -59,9 +77,13 @@ public class BookStore
         // 提示：创建多个 Task 调用 CheckoutAsync，并传入不同书名和数量
         var tasks = new List<Task>
         {
-            
+            CheckoutAsync("C#入门",2),
+            CheckoutAsync("C#入门",3),
+            CheckoutAsync("异步编程",1),
+            CheckoutAsync("异步编程",2),
+            CheckoutAsync("异步编程",3)
         };
-
+        await Task.WhenAll(tasks);
 
         Console.WriteLine("\n购买后库存：");
         books = await _db.GetBooksAsync();
